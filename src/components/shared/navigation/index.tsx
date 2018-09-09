e9c44d7d22801b2c7288b18cd910ee47b86bb3fa
dkriskovic <dario.kriskovic@infobip.com>
2018-09-09 22:28:43 +0200

    navigation changes. remove link, change navbar brand

--- src/components/shared/navigation/index.tsx
+++ src/components/shared/navigation/index.tsx
@@ -1,16 +1,9 @@
 import * as React from 'react'
 import { match } from "react-router"
-import { NavLink } from 'react-router-dom'
 
 import './navigation.css'
 
-import {
-    Collapse,
-    Nav,
-    Navbar,
-    NavbarBrand,
-    NavbarToggler    
-} from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
 
 interface State {
     isOpen: boolean;
@@ -42,18 +35,10 @@ export class Navigation extends React.Component<{}, State> {
             <div>
                 <Navbar color="light" light={true} expand="md">
                     <div className="container">
-                        <NavbarBrand href="/" />
-                        <NavbarToggler onClick={this.toggle} />
-                        <Collapse isOpen={this.state.isOpen} navbar={true}>
-                            <Nav className="ml-auto" navbar={true}>
-                                <NavLink to="/" activeClassName="active" isActive={this.oddEvent}>                                    
-                                    Tournaments
-                                </NavLink>
-                                <NavLink to="/about" activeClassName="active" isActive={this.oddEvent}>                                    
-                                    About
-                                </NavLink>                        
-                            </Nav>
-                        </Collapse>
+                        <NavbarBrand href="/" >
+                            <h2>GLAVNO DA SE IGRA</h2>                            
+                            <p>Tenis liga</p>
+                        </NavbarBrand>                        
                     </div>
                 </Navbar>                
             </div>        
